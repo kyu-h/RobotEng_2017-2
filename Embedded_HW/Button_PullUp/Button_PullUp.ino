ce88ffaab6bbe6624a9205b302aeb6382b246270
kyu-h <tigerk9212@gmail.com>
2017-12-14 14:01:51 +0900

    pull up & pull down of button

---
+++ Embedded_HW/Button_PullUp/Button_PullUp.ino
@@ -0,0 +1,10 @@
+int pushButton =4;
+void setup(){
+  Serial.begin(9600);
+  pinMode(pushButton, INPUT_PULLUP);
+}
+void loop(){
+  int buttonState = digitalRead(pushButton);
+  Serial.println(buttonState);
+  delay(1);
+}
